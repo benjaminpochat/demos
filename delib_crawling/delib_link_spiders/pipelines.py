b4d8f874098894764f44a7f8a1e8ea4eb357d0f1
Benjamin POCHAT <benjamin@MacBookPro.home>
2018-09-24 21:24:20 +0200

    initializing project introducing crawling process for link data gathering

---
+++ delib_crawling/delib_link_spiders/pipelines.py
@@ -0,0 +1,13 @@
+import json
+
+class BulkLinksJsonFileWriterPipeline(object):
+    def open_spider(self, spider):
+        self.file = open('../bulk_data/bulk_links.json', 'w')
+
+    def close_spider(self, spider):
+        self.file.close()
+
+    def process_item(self, item, spider):
+        line = json.dumps(dict(item)) + "\n"
+        self.file.write(line)
+        return item
