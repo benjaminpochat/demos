d154e9a6353a836fc84e2ef981739532d4ff0c63
Benjamin POCHAT <benjamin.pochat@gmail.com>
2018-12-29 18:15:40 +0100

    add command line arguments for configuration

--- src/main/python/commons/configuration.py
+++ src/main/python/commons/configuration.py
@@ -4,13 +4,40 @@ import yaml
 
 
 class Configuration:
+    """
+    A class to handle configuration
+    Every attribute of the configuration is a string
+    """
 
-    def __init__(self):
-        config_file_path = os.path.join(os.path.dirname(__file__),
-                     '../../resources/config.yml')
-        ymlfile = open(config_file_path, 'r')
-        config_dict  = yaml.load(ymlfile)
-        self.database_host = config_dict['database']['host']
-        self.database_port = config_dict['database']['port']
-        self.database_port = config_dict['machine_learning_model']['model_file']
-        self.database_port = config_dict['machine_learning_model']['vocabulary_file']
+    def __init__(self, command_line_arguments: list = []):
+        self.initialize_attributes_from_config_file()
+        self.overload_values_with_command_line_arguments(command_line_arguments)
+        self.convert_attributes_as_strings()
+
+    def initialize_attributes_from_config_file(self):
+        config_file_path = os.path.join(os.path.dirname(__file__), '../../resources/config.yml')
+        yaml_file = open(config_file_path, 'r')
+        config_dict = yaml.load(yaml_file)
+        self.__dict__ = config_dict
+
+    def overload_values_with_command_line_arguments(self, command_line_arguments):
+        i = 0
+        while i < command_line_arguments.__len__():
+            argument_key = command_line_arguments[i][2:]
+            if not self.__dict__.keys().__contains__(argument_key):
+                raise ConfigurationException('Argument \'' + argument_key + '\' is not a configuration item !')
+            if i + 2 > command_line_arguments.__len__():
+                raise ConfigurationException('Argument \'' + argument_key + '\' has no value !')
+            argument_value = command_line_arguments[i + 1]
+            self.__dict__[argument_key] = argument_value
+            i += 2
+
+    def convert_attributes_as_strings(self):
+        for key in self.__dict__.keys():
+            self.__dict__[key] = str(self.__dict__[key])
+
+
+class ConfigurationException(Exception):
+
+    def __init__(self, message: str):
+        self.message = message
--- src/test/python/commons/test_configuration.py
+++ src/test/python/commons/test_configuration.py
@@ -1,5 +1,6 @@
 import unittest
 from src.main.python.commons.configuration import Configuration
+from src.main.python.commons.configuration import ConfigurationException
 
 
 class TestConfiguration(unittest.TestCase):
@@ -12,6 +13,51 @@ class TestConfiguration(unittest.TestCase):
 
         # then
         self.assertEqual(configuration.database_host, 'localhost')
+        self.assertEqual(configuration.database_port, '6379')
+        self.assertEqual(configuration.model_file, 'mlp_model.h5')
+        self.assertEqual(configuration.vocabulary_file, 'vocabulary.pkl')
+
+    def test_init_with_arguments_should_load_primarily_arguments_configuration_values(self):
+        # given
+        args = ['--database_host', 'dbserver', '--database_port', '666']
+
+        # when
+        configuration = Configuration(args)
+
+        # then
+        self.assertEqual(configuration.database_host, 'dbserver')
+        self.assertEqual(configuration.database_port, '666')
+        self.assertEqual(configuration.model_file, 'mlp_model.h5')
+        self.assertEqual(configuration.vocabulary_file, 'vocabulary.pkl')
+
+    def test_init_with_unknown_arguments_should_raise_an_error(self):
+        # given
+        args = ['--xxx', 'yyy']
+        exception = None
+
+        # when
+        try:
+            Configuration(args)
+        except ConfigurationException as e:
+            exception = e
+
+        # then
+        self.assertIsNotNone(exception)
+        self.assertEqual(exception.message, 'Argument \'xxx\' is not a configuration item !')
+
+    def test_init_with_missing_value_should_raise_an_error(self):
+        # given
+        args = ['--database_port', '666', '--database_host']
+
+        # when
+        try:
+            Configuration(args)
+        except ConfigurationException as e:
+            exception = e
+
+        # then
+        self.assertIsNotNone(exception)
+        self.assertEqual(exception.message, 'Argument \'database_host\' has no value !')
 
 if __name__ == '__main__':
     unittest.main()
