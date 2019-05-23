76ebb1c6ce9acc60367c26d48bd2258ed383863c
Benjamin POCHAT <benjamin.pochat@gmail.com>
2019-05-23 21:30:58 +0200

    issue #2 : Permettre un déiement avec Docker

--- scripts/package/build_docker_images.sh
+++ scripts/package/build_docker_images.sh
@@ -2,6 +2,3 @@
 
 # build an image for x86 processor architecture (intel)
 docker build . -f ./src/package/Dockerfile-x86 -t x86/demos:0.0.1
-
-# build an image for arm processor architecture (raspberry)
-docker build . -f ./src/package/Dockerfile-arm -t arm/demos:0.0.1
