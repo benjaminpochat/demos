2627af52317ad5ec58b4c036e7979bd277d00ccd
Benjamin POCHAT <benjamin.pochat@gmail.com>
2020-01-21 00:28:05 +0100

    docker images tagged with the last commit id

---
+++ scripts/build-and-push-images.sh
@@ -0,0 +1,5 @@
+docker-compose -f docker-compose.yml build
+docker-compose -f docker-compose.yml push
+export DOCKER_IMAGE_TAG=$(git log --format="%H" -n 1)
+docker-compose -f docker-compose.yml build
+docker-compose -f docker-compose.yml push
