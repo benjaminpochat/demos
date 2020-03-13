f8c5f0036731d3812e408e27314b211c1e0de383
Benjamin POCHAT <benjamin.pochat@gmail.com>
2020-03-13 21:26:00 +0100

    17 : Optimizing the scraping/classification process : store scraping sessions in the database / restoring scraping statistics

--- scripts/build-and-push-images.sh
+++ scripts/build-and-push-images.sh
@@ -1,6 +1,6 @@
-export DOCKER_IMAGE_TAG=latest
+export DOCKER_IMAGE_TAG=$(git log --format="%H" -n 1)
 docker-compose -f docker-compose.yml build
 docker-compose -f docker-compose.yml push
-export DOCKER_IMAGE_TAG=$(git log --format="%H" -n 1)
+export DOCKER_IMAGE_TAG=latest
 docker-compose -f docker-compose.yml build
 docker-compose -f docker-compose.yml push
