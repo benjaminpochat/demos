aa72a539027c04468df941b7c6ca1625bc18a917
Benjamin POCHAT <benjamin.pochat@gmail.com>
2020-03-30 23:07:30 +0200

    fix build problems du to maven refactoring

--- demos-core/src/test/java/org/demos/core/domains/scraping/TestScrapingSessionController.java
+++ demos-core/src/test/java/org/demos/core/domains/scraping/TestScrapingSessionController.java
@@ -9,11 +9,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Transactional
 public class TestScrapingSessionController {
 
     @Autowired
